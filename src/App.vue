662a4efcaa2192c2e4792bbfe655c4adaa58429f
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2020-04-15 00:23:57 -0400

    Mejorando la interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>COVID - 19</h1>  
+    <p>Pagina web desarrollada para conocer las estadisticas del covid-19 alrededor del mundo </p>  
     <HelloWorld />
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="container py-5"><br><br>
-    <h1 >Estadisticas Globales <span class="badge badge-primary">{{ getDate(datos.Date) }}</span></h1>
+  <div class="container py-5">
+    <h1 >Todos los paises <span class="badge badge-primary">{{ getDate(datos.Date) }}</span></h1>
+    <br><br>
+    <hr style="height: 5px; background: black">
     <br><br>
     <h1>Confirmados</h1>
     <h2 v-for="dato in datos" :key="dato.key" class="">
@@ -21,14 +23,17 @@
       <b>{{ dato.TotalRecovered }}</b>
     </h2>
     <br><br>
-    <h1>Estadisticas por Paises</h1>
-    <br><br>
-    <select class="custom-select" v-model="selected" @change="getPais()">
+    <hr style="height: 5px; background: black">
+    <br><br><br><br><br>
+    <h1>Seleccione su pais: </h1>
+    <br>
+    
+    <select class="custom-select" v-model="selected" @change="getPais()">      
       <option :value="dato.Slug" v-for="dato in datos.Countries" :key="dato.key">{{ dato.Country }}</option>
     </select>
     <br><br>
     <div class="d-flex justify-content-center" v-for="datosPais in datosPaises" :key="datosPais.key">
-      <div class="align-center centrado">
+      <div class="align-center centrado">        
         <br><br>
         <h3>{{ getDate(datosPais.Date) }}</h3>
         <hr style="height: 1px; background: black; width: 405px">
@@ -60,6 +65,10 @@
         <hr style="height: 5px; width:auto; background: black">
       </div>
     </div>
+
+    <footer>
+      Desarrollado por Cristian Marca
+    </footer>
 
   </div>
 </template>

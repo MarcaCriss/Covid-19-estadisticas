d529d1c4862eda680df9fc70e1ecd9a5099cb119
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2020-04-14 23:34:35 -0400

    Primer commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container py-5"><br><br>
+    <h1 >Estadisticas Globales <span class="badge badge-primary">{{ getDate(datos.Date) }}</span></h1>
+    <br><br>
+    <h1>Confirmados</h1>
+    <h2 v-for="dato in datos" :key="dato.key" class="">
+      <b>{{ dato.TotalConfirmed }}</b>
+    </h2>
+    <br><br>
+    <hr style="height: 5px; background: black">
+    <br><br>
+    <h1>Muertos</h1>
+    <h2 v-for="dato in datos" :key="dato.key">
+      <b>{{ dato.TotalDeaths }}</b>
+    </h2>
+    <br><br>
+    <hr style="height: 5px; background: black">
+    <br><br>
+    <h1>Recuperados</h1>
+    <h2 v-for="dato in datos" :key="dato.key">
+      <b>{{ dato.TotalRecovered }}</b>
+    </h2>
+    <br><br>
+    <h1>Estadisticas por Paises</h1>
+    <br><br>
+    <select class="custom-select" v-model="selected" @change="getPais()">
+      <option :value="dato.Slug" v-for="dato in datos.Countries" :key="dato.key">{{ dato.Country }}</option>
+    </select>
+    <br><br>
+    <div class="d-flex justify-content-center" v-for="datosPais in datosPaises" :key="datosPais.key">
+      <div class="align-center centrado">
+        <br><br>
+        <h3>{{ getDate(datosPais.Date) }}</h3>
+        <hr style="height: 1px; background: black; width: 405px">
+        <br>
+        <h1>
+          <b>Confirmados</b>
+        </h1>
+        <hr style="height: 5px; width:250px; background: black">
+          <h2>
+            <b>{{datosPais.Confirmed}}</b>
+          </h2>
+        <br><br><br>
+        <h1 >
+          <b>Muertos</b>
+        </h1>
+        <hr style="height: 5px; width:200px; background: black">
+        <h2>
+          <b>{{ datosPais.Deaths }}</b>
+        </h2>
+        <br><br><br>
+        <h1 >
+          <b>Recuperados</b>
+        </h1>
+        <hr style="height: 5px; width:280px; background: black">
+        <h2>
+          <b>{{ datosPais.Recovered }}</b>
+        </h2>
+        <br><br>
+        <hr style="height: 5px; width:auto; background: black">
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+import Moment from 'moment';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      datos: [],
+      selected: '',
+      datosPaises:[]
+    }
+  },
+  methods: {
+    getDatos() {
+      const path = "https://api.covid19api.com/summary";
+      axios.get(path).then((response) => {
+        this.datos = response.data;
+      })
+    },
+    getDate(date) {
+      Moment.locale('es');
+      return Moment(date).format('D / MMMM / YYYY');
+    },
+    getPais() {
+      const path = `https://api.covid19api.com/live/country/${this.selected}`;
+      axios.get(path).then((response) => {
+        this.datosPaises = response.data;        
+      })
+    }
+  },
+  created() {
+    this.getDatos();
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+option, select {
+  font-size: 25px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+@media only screen and (max-width: 340px) and (min-width: 5px)  {
+  .centrado {
+    align-items: center
+  }
 }
 </style>
